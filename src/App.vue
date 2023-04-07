46e65b065dd3e018d18e1b4c7b03082ee594c5ca
SkyZ0347 <skyjumpzyt@gmail.com>
2023-04-08 00:38:02 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1232.368027365148!2d16.59605281248651!3d51.84751599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4705986a8abf5019%3A0x33b060c7fdd6f767!2sHerbi.%20Firma.%20Rudzka%20E.!5e0!3m2!1spl!2spl!4v1680785365000!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </header>
+</template>
+
+<style scoped>
+header{
+  width: 100%;
+  height: 10vh;
+  background-color: #094067;
+}
+</style>
