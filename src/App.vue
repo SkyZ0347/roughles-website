7945970d823b1fbe9e2a92546cbe72ba01a6c470
SkyZ0347 <skyjumpzyt@gmail.com>
2023-04-08 12:01:57 +0200

    Small style edits

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,12 @@
 
 <template>
   <header>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1232.368027365148!2d16.59605281248651!3d51.84751599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4705986a8abf5019%3A0x33b060c7fdd6f767!2sHerbi.%20Firma.%20Rudzka%20E.!5e0!3m2!1spl!2spl!4v1680785365000!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
   </header>
+  <main>
+    <iframe id="google-maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1232.368027365148!2d16.59605281248651!3d51.84751599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4705986a8abf5019%3A0x33b060c7fdd6f767!2sHerbi.%20Firma.%20Rudzka%20E.!5e0!3m2!1spl!2spl!4v1680785365000!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
+  </main>
 </template>
 
 <style scoped>
@@ -13,5 +17,9 @@ header{
   width: 100%;
   height: 10vh;
   background-color: #094067;
+}
+#google-maps{
+  width: 100%;
+  height: 60vh;
 }
 </style>
